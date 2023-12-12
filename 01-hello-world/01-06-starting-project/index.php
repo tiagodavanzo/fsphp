e2bbdbc9482e649abf3a491bedce11bd6886175c
tiagodavanzo <tiago.davanzo@gmail.com>
2023-12-11 22:58:53 -0300

    Primeiro commit do projeto

---
+++ 01-hello-world/01-06-starting-project/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FSPHP - Starting Project</title>
+</head>
+<body>
+    <?php
+        $start = 'Starting Project!';
+        echo "Hello World! {$start}"; 
+    ?>
+</body>
+</html>
---
+++ 02-iniciando-do-zero-com-php/02-02-definindo-ambiente/index.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.02 - Definindo Ambiente");
+
+/*
+ * 
+ */
+fullStackPHPClassSession("Debug Section", __LINE__);
+
+var_dump($_SERVER);
